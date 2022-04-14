037b8fac49b8e2cc1b10f1adec90f203a1ee61c6
esdandreu <esdandreu@gmail.com>
2022-04-14 19:10:45 +0900

    Moved conftest to test repository

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from pathlib import Path
-
-@pytest.fixture(scope="session")
-def output_folder():
-    output_folder = Path(__file__).parent / "tests" / "output"
-    output_folder.mkdir(exist_ok=True, parents=True)
-    return output_folder
