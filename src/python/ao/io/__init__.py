03b515cd7e562564a8e74326b68d8ef6eed993be
esdandreu <esdandreu@gmail.com>
2022-05-03 14:09:11 +0900

    Some work in progress generating an image dataset

--- src/python/ao/io/__init__.py
+++ src/python/ao/io/__init__.py
@@ -1,1 +1,13 @@
 from .wave_read import wave_read
+import yaml
+
+def yaml_dump(data, to_file: Path = None) -> str:
+    if to_file:
+        with open(to_file, 'w', encoding="utf-8") as f:
+            return yaml.dump(data, stream=f, allow_unicode=True)
+    return yaml.dump(data, allow_unicode=True)
+
+
+def yaml_load(from_file: Path) -> dict:
+    with open(from_file, 'r', encoding="utf-8") as f:
+        return yaml.safe_load(f)
