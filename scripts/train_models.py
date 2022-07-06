f060595f8a5a1e8ee1f7c3a98307f0d12432512f
esdandreu <esdandreu@gmail.com>
2022-07-07 00:35:35 +0900

    train models script

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -12,24 +12,61 @@ from train_model import train_model
 
 import os
 
-from pathlib import Path
 from warnings import warn
 from dotenv import load_dotenv
 
-
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser("Train multiple Acoustic Odometry models")
+    parser.add_argument(
+        '--batch-size',
+        '-b',
+        default=32,
+        type=int,
+        )
+    parser.add_argument(
+        '--gpus',
+        '-g',
+        default=-1,
+        type=int,
+        nargs='+',
+        )
+    args = parser.parse_args()
+
     load_dotenv()
     models_folder = os.environ['MODELS_FOLDER']
     datasets_folder = os.environ['DATASETS_FOLDER']
 
     models_to_train = {
-        'base_cnn': {'dataset': 'base'}
-    }
+        'segment-120': {
+            'dataset': 'segment-120',
+            'validation_split': 0.2,
+            'shard_selection_strategy': 'base',
+            },
+        'duration-05': {
+            'dataset': 'duration-05',
+            'validation_split': 0.2,
+            'shard_selection_strategy': 'base',
+            },
+        'overlap-06': {
+            'dataset': 'overlap-06',
+            'validation_split': 0.2,
+            'shard_selection_strategy': 'base',
+            },
+        'no-negative-slip': {
+            'dataset': 'no-negative-slip',
+            'validation_split': 0.2,
+            'shard_selection_strategy': 'base',
+            },
+        }
     for name, kwargs in models_to_train.items():
         try:
             train_model(
-                dataset_folder=datasets_folder / kwargs.pop('dataset'),
-                output_folder=models_folder / name,
+                name=name,
+                models_folder=models_folder,
+                batch_size=args.batch_size,
+                gpus=args.gpus,
                 **kwargs
                 )
         except ValueError as e:
