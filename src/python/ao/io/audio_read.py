399b804de05bd9341793fb2755012d28fd1482c1
Andreu <esdandreu@gmail.com>
2022-07-18 13:14:37 +0900

    Backend option in audio read

--- src/python/ao/io/audio_read.py
+++ src/python/ao/io/audio_read.py
@@ -24,7 +24,8 @@ def _wave_read(file: Union[str, BinaryIO]) -> Tuple[np.ndarray, int]:
                 np.frombuffer(
                     f.readframes(f.getnframes()),
                     dtype=f'int{f.getsampwidth()*8}'
-                    ), (-1, f.getnchannels()),
+                    ),
+                (-1, f.getnchannels()),
                 ).T.astype(np.float64, order='F'),
             f.getframerate(),
             )
@@ -57,6 +58,7 @@ def _audio_read(
 
 def audio_read(
     path: Union[str, Path],
+    backend: str = 'wave',
     **kwargs,
     ) -> Tuple[np.ndarray, int]:
     """Reads content of an audio file into a floating point numpy array.
@@ -66,29 +68,30 @@ def audio_read(
             be downloaded. If it points to a file in the system, this will be
             opened and read. If it points to a web URL, it will be downloaded
             to a temporary file which will be opened and read.
-        resample_to (int, optional): If provided, audio will automatically be
-            resampled to the given rate. Defaults to None.
-        mono (bool, optional): If True, the signal will be converted to mono.
-        kwargs (dict): Keyword arguments to pass to librosa.load.
+        backend (str, optional): Backend to use for reading the audio file.
+            Whether `wave` or `librosa`.
+        kwargs (dict): Keyword arguments to pass to the selected backend.
 
     Returns:
         Tuple[np.ndarray, int]: Tuple containing the signal array and sampling
         rate.
     """
+    if backend == 'wave':
+        read_fn  = _wave_read
+    elif backend == 'librosa':
+        read_fn  = _audio_read
+    else:
+        raise ValueError(f'Unknown backend: {backend}')
     str_path = str(path)
     if Path(path).exists():  # If it is a file found in the system
-        return _wave_read(str_path)
-        # return _audio_read(path, resample_to=resample_to, mono=mono, **kwargs)
+        return read_fn(str_path, **kwargs)
     urlparsed = urlparse(str_path)
     if all([urlparsed.scheme, urlparsed.netloc]):  # If it is a valid URL
         with tempfile.TemporaryFile() as temp:
             content = requests.get(str_path).content
             temp.write(content)
             temp.seek(0)
-            return _wave_read(temp)
-            # return _audio_read(
-            #     temp, resample_to=resample_to, mono=mono, **kwargs
-            #     )
+            return read_fn(temp, **kwargs)
     raise ValueError(
         f'{str_path} is neither a URL nor a file found in the system'
         )
