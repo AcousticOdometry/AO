6166f8b7e1118944693bee5c15eb9a240fbe85dc
esdandreu <esdandreu@gmail.com>
2022-07-20 00:57:42 +0900

    training everything

--- adhoc/models/ordinal_classification.py
+++ adhoc/models/ordinal_classification.py
@@ -54,8 +54,64 @@ class OrdinalClassificationBase(AcousticOdometryBase):
     def get_label(self, sample: dict) -> torch.tensor:
         return torch.bucketize(sample['Vx'], boundaries=self.boundaries)
 
+    def ordinal_loss(self, prediction: torch.tensor, y: torch.tensor):
+        # Create out encoded target with [batch_size, num_labels] shape
+        encoded_y = torch.zeros_like(prediction)
+        # Fill in ordinal target function, i.e. 1 -> [1,1,0,...]
+        for i, label in enumerate(y):
+            encoded_y[i, 0:label + 1] = 1
+        return self.cost_function(prediction, encoded_y).sum(axis=1).mean()
 
-class CNN(AcousticOdometryBase):
+    def _shared_eval_step(self, batch, batch_idx):
+        x, y = batch
+        prediction = self(x.float())
+        return {
+            'loss': self.ordinal_loss(prediction, y),
+            'acc': accuracy(self.decode_label(prediction), y)
+            }
+
+
+class UnnormalizedCNN(OrdinalClassificationBase):
+
+    def __init__(
+        self,
+        input_dim: Tuple[int, int, int],
+        lr: float = 0.0001,
+        boundaries: np.ndarray = np.linspace(0.005, 0.065, 7),
+        conv1_filters: int = 64,
+        conv1_size: int = 5,
+        conv2_filters: int = 128,
+        conv2_size: int = 5,
+        hidden_size: int = 512,
+        ):
+        super().__init__(input_dim, lr, boundaries)
+        self.save_hyperparameters()
+        self.conv1 = nn.Conv2d(
+            input_dim[0], conv1_filters, kernel_size=conv1_size
+            )
+        self.conv2 = nn.Conv2d(
+            conv1_filters, conv2_filters, kernel_size=conv2_size
+            )
+        self.conv2_drop = nn.Dropout2d()
+        self.flatten = nn.Flatten()
+        fc1_input = torch.numel(self._forward(torch.zeros((1, *input_dim))))
+        self.fc1 = nn.Linear(fc1_input, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, self.output_dim)
+
+    def _forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        return self.flatten(x)
+
+    def forward(self, x):
+        x = self._forward(x)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc2(x))
+        return torch.sigmoid(x)
+
+
+class CNN(OrdinalClassificationBase):
 
     def __init__(
         self,
@@ -81,7 +137,7 @@ class CNN(AcousticOdometryBase):
         self.flatten = nn.Flatten()
         fc1_input = torch.numel(self._forward(torch.zeros((1, *input_dim))))
         self.fc1 = nn.Linear(fc1_input, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, self.hparams['output_dim'])
+        self.fc2 = nn.Linear(hidden_size, self.output_dim)
 
     def _forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -95,19 +151,3 @@ class CNN(AcousticOdometryBase):
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
         return torch.sigmoid(x)
-
-    def ordinal_loss(self, prediction: torch.tensor, y: torch.tensor):
-        # Create out encoded target with [batch_size, num_labels] shape
-        encoded_y = torch.zeros_like(prediction)
-        # Fill in ordinal target function, i.e. 1 -> [1,1,0,...]
-        for i, label in enumerate(y):
-            encoded_y[i, 0:label + 1] = 1
-        return self.cost_function(prediction, encoded_y).sum(axis=1).mean()
-
-    def _shared_eval_step(self, batch, batch_idx):
-        x, y = batch
-        prediction = self(x.float())
-        return {
-            'loss': self.ordinal_loss(prediction, y),
-            'acc': accuracy(self.decode_label(prediction), y)
-            }
--- adhoc/train_models.py
+++ adhoc/train_models.py
@@ -49,30 +49,167 @@ if __name__ == '__main__':
     models_folder = os.environ['MODELS_FOLDER']
     datasets_folder = os.environ['DATASETS_FOLDER']
 
-    models_to_train= {}
-    for seed in range(15, 30):
-        models_to_train[f'unnormalized-{seed}'] = {
+    if args.gpu == 0:
+        # Base
+        # Smaller size 3
+        # More bins 2
+        models_to_train = {
+            'base': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 64,
+                'conv1_size': 5,
+                'conv2_filters': 128,
+                'conv2_size': 5,
+                'hidden_size': 512,
+                },
+            'base-M': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 32,
+                'conv1_size': 5,
+                'conv2_filters': 64,
+                'conv2_size': 5,
+                'hidden_size': 256,
+                },
+            'base-S': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 16,
+                'conv1_size': 5,
+                'conv2_filters': 32,
+                'conv2_size': 5,
+                'hidden_size': 256,
+                },
+            'base-XS': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 8,
+                'conv1_size': 5,
+                'conv2_filters': 16,
+                'conv2_size': 5,
+                'hidden_size': 128,
+                },
+            'base-bins-14': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 14),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 64,
+                'conv1_size': 5,
+                'conv2_filters': 128,
+                'conv2_size': 5,
+                'hidden_size': 512,
+                },
+            'base-bins-28': {
+                'dataset': 'base',
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 28),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 64,
+                'conv1_size': 5,
+                'conv2_filters': 128,
+                'conv2_size': 5,
+                'hidden_size': 512,
+                },
+            }
+    elif args.gpu == 1:
+        # Datasets 6 models
+        models_to_train = {}
+        for dataset in [
+            'duration-5', 'duration-20', 'features-64', 'features-128',
+            'segment-100', 'segment-140'
+            ]:
+            models_to_train[dataset] = {
+                'dataset': dataset,
+                'split_strategy': 'no-laptop',
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 64,
+                'conv1_size': 5,
+                'conv2_filters': 128,
+                'conv2_size': 5,
+                'hidden_size': 512,
+                }
+    elif args.gpu == 2:
+        # Split strategies and data agumentation 7
+        models_to_train = {}
+        for split_strategy in [
+            'with-laptop', 'only-videomic', 'no-negative-slip', 'all-devices',
+            'with-noise', 'with-gain', 'all-transforms'
+            ]:
+            models_to_train[split_strategy] = {
+                'dataset': 'base',
+                'split_strategy': split_strategy,
+                'task': 'classification',
+                'boundaries': np.linspace(0.005, 0.065, 7),
+                'architecture': 'UnnormalizedCNN',
+                'conv1_filters': 64,
+                'conv1_size': 5,
+                'conv2_filters': 128,
+                'conv2_size': 5,
+                'hidden_size': 512,
+                }
+    elif args.gpu == 3:
+        # Ordinal classification 2
+        # Regression 2
+        # Unnormalized
+        # Informed
+        models_to_train = {}
+        models_to_train['base-batchnorm'] = {
             'dataset': 'base',
             'split_strategy': 'no-laptop',
             'task': 'classification',
             'boundaries': np.linspace(0.005, 0.065, 7),
-            'architecture': 'UnnormalizedCNN',
+            'architecture': 'CNN',
             'conv1_filters': 64,
             'conv1_size': 5,
             'conv2_filters': 128,
             'conv2_size': 5,
             'hidden_size': 512,
-            'seed': seed,
             }
-    # models_to_train = {
-    #     'squeeze-net': {
-    #         'dataset': 'base',
-    #         'split_strategy': 'no-laptop',
-    #         'task': 'classification',
-    #         'boundaries': np.linspace(0.005, 0.065, 7),
-    #         'architecture': 'SqueezeNet',
-    #         },
-    #     }
+        for task, task_kwargs in zip(['regression', 'ordinal_classification'],
+                                     [{}, {
+                                         'boundaries':
+                                             np.linspace(0.005, 0.065, 7)
+                                         }]):
+            for suffix, architecture in zip(['', '-batchnorm'],
+                                            ['UnnormalizedCNN', 'CNN']):
+                models_to_train[task.replace('_', '-') + suffix] = {
+                    'dataset': 'base',
+                    'split_strategy': 'no-laptop',
+                    'task': task,
+                    'architecture': architecture,
+                    'conv1_filters': 64,
+                    'conv1_size': 5,
+                    'conv2_filters': 128,
+                    'conv2_size': 5,
+                    'hidden_size': 512,
+                    **task_kwargs
+                    }
+        models_to_train['informed'] = {
+            'dataset': 'base',
+            'split_strategy': 'no-laptop',
+            'task': 'classification',
+            'architecture': 'UnnormalizedInformedCNN',
+            }
+    else:
+        raise ValueError(f"Invalid gpu: {args.gpu}")
     for name, kwargs in models_to_train.items():
         if args.datasets and kwargs['dataset'] not in args.datasets:
             print(f"Skipping model {name} for dataset {kwargs['dataset']}")
@@ -87,7 +224,7 @@ if __name__ == '__main__':
                 **{
                     'min_epochs': 20,
                     'max_epochs': 30,
-                    'seed': 1,
+                    'seed': 2,
                     **kwargs
                     }
                 )
